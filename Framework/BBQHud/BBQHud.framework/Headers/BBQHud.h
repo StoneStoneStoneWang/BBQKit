5007e7584084f4d16d743a24e52b9cdc75f7cb1b
StoneStoneStoneWang <yuanxingfu1314@163.com>
2020-03-28 17:06:53 +0800

    BBQ

---
+++ Framework/BBQHud/BBQHud.framework/Headers/BBQHud.h
@@ -0,0 +1,19 @@
+//
+//  BBQHud.h
+//  BBQHud
+//
+//  Created by 王磊 on 2020/3/28.
+//  Copyright © 2020 王磊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BBQHud.
+FOUNDATION_EXPORT double BBQHudVersionNumber;
+
+//! Project version string for BBQHud.
+FOUNDATION_EXPORT const unsigned char BBQHudVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBQHud/PublicHeader.h>
+
+#import <BBQHud/BBQHudUtil.h>
---
+++ Framework/BBQYYCache/BBQYYCache.framework/Headers/BBQYYCache.h
@@ -0,0 +1,20 @@
+//
+//  BBQYYCache.h
+//  BBQYYCache
+//
+//  Created by 王磊 on 2020/3/28.
+//  Copyright © 2020 王磊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BBQYYCache.
+FOUNDATION_EXPORT double BBQYYCacheVersionNumber;
+
+//! Project version string for BBQYYCache.
+FOUNDATION_EXPORT const unsigned char BBQYYCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBQYYCache/PublicHeader.h>
+
+#import <BBQYYCache/ZCacheUtil.h>
+
