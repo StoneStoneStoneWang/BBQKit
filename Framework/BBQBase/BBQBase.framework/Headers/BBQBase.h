a12264d3b0e05837b8276b45109dbb2589309b02
StoneStoneStoneWang <yuanxingfu1314@163.com>
2020-03-28 11:13:17 +0800

    BBQBase

---
+++ Framework/BBQBase/BBQBase.framework/Headers/BBQBase.h
@@ -0,0 +1,19 @@
+//
+//  BBQBase.h
+//  BBQBase
+//
+//  Created by 王磊 on 2020/3/28.
+//  Copyright © 2020 王磊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BBQBase.
+FOUNDATION_EXPORT double BBQBaseVersionNumber;
+
+//! Project version string for BBQBase.
+FOUNDATION_EXPORT const unsigned char BBQBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBQBase/PublicHeader.h>
+
+#import <BBQBase/BBQBaseViewController.h>
